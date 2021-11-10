88c8c43194ecb50419b6a83bb4fc350df216ef6c
Anton Gusev <FrostTG@gmail.com>
2021-11-10 21:29:11 +0300

    Добавьте файлы проекта.

---
+++ Home3WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Home3WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (tBox != null)
+            {
+                tBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (tBox != null)
+            {
+                tBox.FontSize = Convert.ToDouble(fontSize);
+            }
+        }
+        private void bold_Click(object sender, RoutedEventArgs e)
+        {
+            if (tBox.FontWeight!=FontWeights.Bold)
+            {
+                tBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                tBox.FontWeight = FontWeights.Normal;
+            }
+            
+        }
+
+        private void italic_Click(object sender, RoutedEventArgs e)
+        {
+            if (tBox.FontStyle != FontStyles.Italic)
+            {                
+                tBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                tBox.FontStyle = FontStyles.Normal;
+            }            
+        }
+
+        private void underline_Click(object sender, RoutedEventArgs e)
+        {
+            if (tBox.TextDecorations != TextDecorations.Underline)
+            {
+                tBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                tBox.TextDecorations = null;
+            }
+        }
+        private void black_Click(object sender, RoutedEventArgs e)
+        {
+            tBox.Foreground = Brushes.Black;
+        }
+
+        private void red_Click(object sender, RoutedEventArgs e)
+        {
+            tBox.Foreground = Brushes.Red;
+        }
+    }
+}
